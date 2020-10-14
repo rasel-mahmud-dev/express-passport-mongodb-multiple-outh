f44e3cb97e6952c351797de609cf37812faf166c
RaseL Mahmud <42914006+rasel-reactive@users.noreply.github.com>
2020-10-15 01:13:14 +0600

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -3,13 +3,12 @@
 module.exports = {
   mongoURI: "mongodb://localhost/express-passport-maltiple-auth",
   
-  facebookClientID: "556742011549824",
-  facebookClientSecret: "1b405ddfb66dd5bd2586d576df03c279",
+  facebookClientID: "",
+  facebookClientSecret: "",
   facebookCallbackUrl: "/auth/facebook/callback",
 
-  googelClientSecret: "kuO0riyT8mywX_cfBvKw8A2r",
-  googelClientID:
-  "169802712538-gpu37tkikd5cuggovtgr30pfm22irbeb.apps.googleusercontent.com",
-  cookieSecret: "FFF:DFOie0fieffffvccc",
+  googelClientSecret: "r",
+  googelClientID:"-.apps..com",
+  cookieSecret: "FFF:",
   googleCallbackUrl: "/auth/google/callback",
 };
